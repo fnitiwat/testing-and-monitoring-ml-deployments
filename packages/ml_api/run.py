13b68195618c3b03bbface9cc6cf5aea0d9426ff
nitiwat sompawong <nitiwat.sompawong@aigencorp.com>
2021-11-12 01:29:06 +0700

    implort controller in run, fix 404

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -3,6 +3,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from api.app import create_app
 from api.config import DevelopmentConfig, setup_app_logging
+from api import controller
 
 _config = DevelopmentConfig()
 
@@ -13,7 +14,6 @@ application = DispatcherMiddleware(
         app=main_app.wsgi_app,
         mounts={'/metrics': prometheus_client.make_wsgi_app()}
     )
-
 
 if __name__ == "__main__":
     main_app.run(port=_config.SERVER_PORT, host=_config.SERVER_HOST)
